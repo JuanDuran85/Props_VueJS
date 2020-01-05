6437b3d31a7006c0f4521c9f84f791317eddec6e
JuanDuran85 <duran85juan@gmail.com>
2020-01-05 16:26:04 -0300

    agregando componente

---
+++ componentes/ComponentesProps.js
@@ -0,0 +1,12 @@
+Vue.component('componentes-props', {
+    template: `
+        <div>
+            <h3>Mensaje desde componente</h3>
+        </div>
+    `,
+    data() {
+        return {
+            
+        }
+    },
+})
